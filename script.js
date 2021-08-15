7bd6fe4c9ec6b9801530e3dc0237674600afedb7
Igor <57453357+misterio-smit@users.noreply.github.com>
2021-08-15 20:20:18 +0700

    сделал вызов программы рекурсивно

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
 "use strict";
 
 let userNum;
+let game = function () {
+  //Загадываем число от 0 до 100
+  let numBot = (Math.floor(Math.random() * 100) + 1);
 
-//Загадываем число от 0 до 100
-let numBot = (Math.floor(Math.random() * 100) + 1);
-console.log(numBot);
-
-let isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-//Получаем число от пользователя
-let start = function () {
-
-  userNum = +prompt('Угадай число от 1 до 100');
-
-  while (!isNumber(userNum)) {
-
-    userNum = +prompt('Введи число!');
-  }
-  if (userNum === 0) {
-    alert('Игра окончена!');
-    return;
-  }
-  let guess = function () {
-
-    if (userNum === numBot) {
-      alert('Поздравляю, Вы угадали!!!');
-      return;
-    } else if (userNum > numBot) {
-      alert('Загаданное число меньше');
-      return start();
-    } else if (userNum < numBot) {
-      alert('Загаданное число больше');
-      return start();
-    }
+  let isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
   };
-  console.log(userNum);
-  guess();
-};
 
-start();
+  //Получаем число от пользователя
+  let start = function () {
+    userNum = +prompt('Угадай число от 1 до 100');
+    while (!isNumber(userNum)) {
+      userNum = +prompt('Введи число!');
+    }
+    if (userNum === 0) {
+      alert('Игра окончена!');
+      return;
+    }
+    let guess = function () {
+
+      if (userNum === numBot) {
+        alert('Поздравляю, Вы угадали!!!');
+        return;
+      } else if (userNum > numBot) {
+        alert('Загаданное число меньше');
+        game();
+      } else if (userNum < numBot) {
+        alert('Загаданное число больше');
+        game();
+      }
+    };
+    guess();
+  };
+  start();
+};
+game();
